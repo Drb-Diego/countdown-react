a825318d0275bfece0fc370da62018583a70923a
Diego Brito <diegorbrito9@gmail.com>
2021-12-30 23:15:25 -0300

    Criando a funcionalidade de incremenetar e decrementar o tempo

--- src/components/CardClock.jsx
+++ src/components/CardClock.jsx
@@ -7,33 +7,43 @@ import { FaCaretLeft } from "react-icons/fa";
 import { FaCaretRight } from "react-icons/fa";
 
 export default function CardClock() {
-  const { hour, minutes, seconds } = useContext(context);
+  const { 
+    hour, 
+    minutes, 
+    seconds,
+    handleIncrementHour,
+    handleDecrementHour,
+    handleIncrementMinutes,
+    handleDecrementMinutes,
+    handleIncrementSeconds,
+    handleDecrementSeconds,
+  } = useContext(context);
 
   return (
     <>
       <div className='card-container'>
-        <div className='card-content' name='hour'>
-          <FaCaretLeft className='increment' />
-          <h1>{hour}</h1>
-          <FaCaretRight className="decrement" />
+        <div className='card-content' >
+          <FaCaretLeft className='increment' onClick={ handleIncrementHour } />
+          <h1>{hour === 0 ? '00' : hour }</h1>
+          <FaCaretRight className="decrement" onClick={ handleDecrementHour } />
         </div>
         <p>Hora</p>
       </div>
 
       <div className='card-container'>
-        <div className='card-content' name='hour'>
-          <FaCaretLeft className='increment' />
-          <h1>{minutes}</h1>
-          <FaCaretRight className="decrement" />
+        <div className='card-content' >
+          <FaCaretLeft className='increment' onClick={handleIncrementMinutes} />
+          <h1>{minutes === 0 ? '00' : minutes}</h1>
+          <FaCaretRight className="decrement" onClick={ handleDecrementMinutes } />
         </div>
         <p>Minutos</p>
       </div>
 
       <div className='card-container'>
-        <div className='card-content' name='hour'>
-          <FaCaretLeft className='increment' />
-          <h1>{seconds}</h1>
-          <FaCaretRight className="decrement" />
+        <div className='card-content' >
+          <FaCaretLeft className='increment' onClick={ handleIncrementSeconds } />
+          <h1>{seconds === 0 ? '00' : seconds}</h1>
+          <FaCaretRight className='decrement' onClick={ handleDecrementSeconds } />
         </div>
         <p>Segundos</p>
       </div>
--- src/context/clockContext.js
+++ src/context/clockContext.js
@@ -2,17 +2,55 @@ import React, { useState } from 'react'
 import Context from './context'
 
 export default function Provider({ children }) {
-  const [hour, setHour] = useState('00');
-  const [minutes, setMinutes] = useState('00');
-  const [seconds, setSeconds] = useState('00');
+  const [hour, setHour] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+
+  const handleIncrementHour = () => {
+    if(hour < 23) setHour(hour + 1);
+  }
+
+  const handleDecrementHour = () => {
+    if(hour > 0) setHour(hour - 1);
+  }
+
+  const handleIncrementMinutes = () => {
+    if(minutes < 59)  setMinutes(minutes + 1);
+
+    else {
+      if (hour < 23) setHour(hour + 1);
+      setMinutes(0);
+    }
+  }
+
+  const handleDecrementMinutes = () => {
+    if(minutes > 0) setMinutes(minutes - 1);
+  }
+
+  const handleIncrementSeconds = () => {
+    if(seconds < 59)  setSeconds(seconds + 1);
+
+    else {
+      if (minutes < 59) setMinutes(minutes + 1);
+      setSeconds(0);
+    }
+  }
+
+  const handleDecrementSeconds = () => {
+    if(seconds > 0) setSeconds(seconds - 1);
+  }
 
   const providerValue = {
     hour,
-    setHour,
+    handleIncrementHour,
+    handleDecrementHour,
     minutes,
-    setMinutes,
+    handleIncrementMinutes,
+    handleDecrementMinutes,
     seconds,
-    setSeconds
+    handleIncrementSeconds,
+    handleDecrementSeconds,
   }
 
   return (
